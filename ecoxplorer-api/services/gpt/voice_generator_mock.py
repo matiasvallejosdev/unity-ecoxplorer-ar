73707150a6e8c4f53e19a4a996247cf5ef67045b
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-11-09 17:53:32 -0300

    Fix voice_generator_mock it was not setted properly.

--- ecoxplorer-api/services/gpt/voice_generator_mock.py
+++ ecoxplorer-api/services/gpt/voice_generator_mock.py
@@ -6,7 +6,7 @@ from common.utils.response_utils import success_response
 def lambda_handler(event, context):
     voice_generator_mock = {
         "voice_id": "76db0bd0-df4a-4413-895e-c85b922d5ae2",
-        "voice_story_output": "https://ecoxplorer-dev.s3.us-east-1.amazonaws.com/storyteller/tts_audio_76db0bd0-df4a-4413-895e-c85b922d5ae2.mp3",
+        "voice_story_output": "https://ecoxplorer-dev.s3.us-east-1.amazonaws.com/voices/tts_audio_76db0bd0-df4a-4413-895e-c85b922d5ae2.mp3",
         "voice": "fable",
     }
     return success_response(voice_generator_mock)
